31deaf3b4e37a869fb6c401ac4ae5f403ff0bfaf
Desmond235 <adabedesmond@gmail.com>
2024-02-21 23:43:18 -0800

    added search_screen

--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/database/note_database.dart';
+import 'package:note_app/model/note_model.dart';
+import 'package:note_app/widgets/search_bar.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -10,20 +12,79 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   NoteDatabase noteDatabase = NoteDatabase.instance;
+  List<NoteModel> notes = [];
+
+  void searchNote(String text) async {
+    final noteItems = await noteDatabase.search(text);
+    setState(() {
+      notes = noteItems;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Padding(
-        padding:const EdgeInsets.all(10),
-        child:Container(
-          margin:  const EdgeInsets.only(top: 20),
-          child: const Column(
-            children: [
-              SearchBar()
-            ],
-          ),
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.only(top: 50),
+        child: Column(
+          children: [
+            SearchBars(
+              onQueryChandged: searchNote,
+            ),
+            // const SizedBox(height: 10),
+            Expanded(
+              child: ListView.builder(
+                itemCount: notes.length,
+                itemBuilder: (context, index) {
+                  final note = notes[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Card(
+                        color: Theme.of(context).colorScheme.secondaryContainer,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                note.title,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyLarge!
+                                    .copyWith(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                note.content.trim(),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(color: Colors.black),
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                note.createdTime.toString().split(' ')[0],
+                                maxLines: 1,
+                                style: TextStyle(
+                                    color: Colors.black.withOpacity(0.6)),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
         ),
-        ),
+      ),
     );
   }
 }
