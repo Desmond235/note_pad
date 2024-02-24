5f638d48275af8a27703ed44db8f27379fe4f6ac
Desmond235 <adabedesmond@gmail.com>
2024-02-24 01:03:23 -0800

    modified file

--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/database/note_database.dart';
 import 'package:note_app/model/note_model.dart';
+import 'package:note_app/screen/note_details_view.dart';
 import 'package:note_app/widgets/search_bar.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -13,16 +14,105 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   NoteDatabase noteDatabase = NoteDatabase.instance;
   List<NoteModel> notes = [];
+  bool noteFound = true;
 
   void searchNote(String text) async {
     final noteItems = await noteDatabase.search(text);
+    if (noteItems.isEmpty) {
+      setState(() {
+        noteFound = false;
+      });
+    } else {
+      noteFound = true;
+    }
     setState(() {
       notes = noteItems;
     });
   }
 
+  void goToNoteDetailsView({int? id}) async {
+    await Navigator.of(context).push<NoteDetailsView>(
+      MaterialPageRoute(
+        builder: ((context) => NoteDetailsView(noteId: id)),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemCount: notes.length,
+      itemBuilder: (context, index) {
+        final note = notes[index];
+        return Padding(
+          padding: const EdgeInsets.all(10),
+          child: GestureDetector(
+            onTap: () => goToNoteDetailsView(id: note.id),
+            child: Card(
+              color: Theme.of(context).colorScheme.secondaryContainer,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      note.title,
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Colors.black, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      note.content.trim(),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyMedium!
+                          .copyWith(color: Colors.black),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      note.createdTime.toString().split(' ')[0],
+                      maxLines: 1,
+                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+
+    if (!noteFound) {
+      content = Column(
+        children: [
+          const SizedBox(height: 20),
+          Container(
+            padding: const EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(40),
+              color: const Color.fromARGB(255, 65, 64, 64),
+            ),
+            child: IconButton(
+              onPressed: null,
+              icon: Icon(
+                Icons.search,
+                color: Colors.white.withOpacity(0.7),
+                size: 50,
+              ),
+            ),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            'Note not found',
+            style:
+                TextStyle(color: Colors.white.withOpacity(0.7), fontSize: 18),
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
       body: Container(
         margin: const EdgeInsets.only(top: 50),
@@ -31,57 +121,8 @@ class _SearchScreenState extends State<SearchScreen> {
             SearchBars(
               onQueryChandged: searchNote,
             ),
-            // const SizedBox(height: 10),
-            Expanded(
-              child: ListView.builder(
-                itemCount: notes.length,
-                itemBuilder: (context, index) {
-                  final note = notes[index];
-                  return Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: GestureDetector(
-                      onTap: () {},
-                      child: Card(
-                        color: Theme.of(context).colorScheme.secondaryContainer,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                note.title,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyLarge!
-                                    .copyWith(
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                note.content.trim(),
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyMedium!
-                                    .copyWith(color: Colors.black),
-                              ),
-                              const SizedBox(height: 10),
-                              Text(
-                                note.createdTime.toString().split(' ')[0],
-                                maxLines: 1,
-                                style: TextStyle(
-                                    color: Colors.black.withOpacity(0.6)),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            )
+            // const SizedBox(height: 10)
+            Expanded(child: content)
           ],
         ),
       ),
