8edd90f3c2a445c7ad67e418f6f4c7ec3798286c
Desmond235 <adabedesmond@gmail.com>
2024-02-27 09:59:33 -0800

    modified file

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -6,20 +6,15 @@ class SearchBars extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    FocusNode focusNode = FocusNode();
     return Container(
       margin: const EdgeInsets.all(5),
       child: Padding(
         padding: const EdgeInsets.all(15),
         child: TextField(
-          focusNode: focusNode,
           cursorColor: Colors.white,
           style: const TextStyle(color: Colors.white),
           textInputAction: TextInputAction.search,
           onChanged: onQueryChandged,
-          onSubmitted: (value) {
-            focusNode.unfocus();
-          },
           decoration: InputDecoration(
             focusedBorder: OutlineInputBorder(
               borderSide: BorderSide(color: Colors.white.withOpacity(0.7)),
